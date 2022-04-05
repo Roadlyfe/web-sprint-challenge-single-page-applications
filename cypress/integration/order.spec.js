e351700eb7da4ed88334cd93ef7163053e21d863
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-04-05 11:37:30 -0500

    all tests at one hundred percent

--- cypress/integration/order.spec.js
+++ cypress/integration/order.spec.js
@@ -1,3 +1,10 @@
 it("sanity check", ()=> {
     expect(8+5).to.equal(13)
 })
+
+describe("pizza website", () => { 
+    cy.visit("http://localhost:3000/")
+})
+it("can visit the home page", () => {
+    cy.contains("Bloomtech Eats!")
+})
